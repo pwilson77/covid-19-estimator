1da5147c97a067a0d0f836b37051de6ea2e64361
pwilson77 <p.o.yirenkyi@gmail.com>
2020-04-13 17:02:28 +0000

    finished the first challenge

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,27 @@
-const covid19ImpactEstimator = data => {
+/* eslint linebreak-style: ["error", "windows"] */
 
+const covid19ImpactEstimator = data => {
+  const {region, periodType, timeToElapse, population, totalHospitalBeds, reportedCases } = data;
+  
+  let currentlyInfected = reportedCases * 10;
+  let severeImpact = { currentlyInfected: reportedCases * 50 };
+  
+  let powerFactor;
+  if (periodType === "days") {
+    powerFactor = parseInt(timeToElapse / 3, 10);
+  } else if (periodType === "weeks") {
+      powerFactor = parseInt((timeToElapse * 7) / 3, 10 );
+  } else {
+    powerFactor = parseInt((timeToElapse * 30) / 3, 10 );
+  }
+
+  let infectionsByRequestedTime = {
+    impact: currentlyInfected * 2 ** powerFactor,
+    severeImpact: severeImpact.currentlyInfected * 2 ** powerFactor,
+  };
+
+   let severeCasesByRequestedTime = parseInt((15 / 100) * infectionsByRequestedTime.severeImpact, 10);
+   let hospitalBedsByRequestedTime = 
 };
 
 export default covid19ImpactEstimator;
