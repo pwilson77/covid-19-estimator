5ed86f68f828a04f7f4e4f1d49d114a583776042
pwilson77 <p.o.yirenkyi@gmail.com>
2020-04-13 17:53:31 +0000

    finished the third challenge and refactored the code to meet return parameters

--- src/estimator.js
+++ src/estimator.js
@@ -1,30 +1,45 @@
+/* eslint-disable max-len */
 /* eslint linebreak-style: ["error", "windows"] */
 
-const covid19ImpactEstimator = data => {
-  const {region, periodType, timeToElapse, population, totalHospitalBeds, reportedCases } = data;
+const covid19ImpactEstimator = (data) => {
+  const {
+    region, periodType, timeToElapse, population, totalHospitalBeds, reportedCases,
+  } = data;
 
-  let currentlyInfected = reportedCases * 10;
-  let severeImpact = { currentlyInfected: reportedCases * 50 };
+  const impact = { currentlyInfected: reportedCases * 10};
+  const severeImpact = { currentlyInfected: reportedCases * 50 };
 
   let powerFactor;
   if (periodType === 'days') {
     powerFactor = parseInt(timeToElapse / 3, 10);
-  } else if (periodType === '"weeks"') {
+  } else if (periodType === 'weeks') {
     powerFactor = parseInt((timeToElapse * 7) / 3, 10);
   } else {
     powerFactor = parseInt((timeToElapse * 30) / 3, 10);
   }
 
-  const infectionsByRequestedTime = {
-    impact: currentlyInfected * 2 ** powerFactor,
-    severeImpact: severeImpact.currentlyInfected * 2 ** powerFactor,
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** powerFactor;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 2 ** powerFactor;
+
+  impact.severeCasesByRequestedTime = parseInt(0.15 * impact.infectionsByRequestedTime, 10);
+  severeImpact.severeCasesByRequestedTime = parseInt(0.15 * severeImpact.infectionsByRequestedTime, 10);
+
+  const dedicatedBeds = parseInt(0.35 * totalHospitalBeds, 10);
+  impact.hospitalBedsByRequestedTime = dedicatedBeds - impact.severeCasesByRequestedTime;
+  severeImpact.hospitalBedsByRequestedTime = dedicatedBeds - severeImpact.severeCasesByRequestedTime;
+
+  impact.casesForICUByRequestedTime = parseInt(0.05 * impact.infectionsByRequestedTime, 10);
+  severeImpact.casesForICUByRequestedTime = parseInt(0.05 * severeImpact.infectionsByRequestedTime, 10);
+
+  impact.casesForVentilatorsByRequestedTime = parseInt(0.02 * impact.infectionsByRequestedTime, 10);
+  severeImpact.casesForVentilatorsByRequestedTime = parseInt(0.02 * severeImpact.infectionsByRequestedTime, 10);
+
+  impact.dollarsInFlight = parseInt((impact.infectionsByRequestedTime * region.avgDailyIncomeInUSD * region.avgDailyIncomePopulation) / 30, 10);
+  severeImpact.dollarsInFlight = parseInt((severeImpact.infectionsByRequestedTime * region.avgDailyIncomeInUSD * region.avgDailyIncomePopulation) / 30, 10);
+
+  return {
+    data, impact, severeImpact,
   };
-
-  const severeCasesByRequestedTime = parseInt((15 / 100) * infectionsByRequestedTime.severeImpact, 10);
-  const dedicatedBeds = parseInt((35 / 100) * totalHospitalBeds, 10);
-  const hospitalBedsByRequestedTime = dedicatedBeds - severeCasesByRequestedTime;
-
-
 };
 
 export default covid19ImpactEstimator;
