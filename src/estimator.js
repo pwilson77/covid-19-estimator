261bd50f7cab676dfecd5f1f7a3892098cbb7dcd
pwilson77 <p.o.yirenkyi@gmail.com>
2020-04-13 17:11:19 +0000

    finished the second challenge

--- src/estimator.js
+++ src/estimator.js
@@ -2,26 +2,29 @@
 
 const covid19ImpactEstimator = data => {
   const {region, periodType, timeToElapse, population, totalHospitalBeds, reportedCases } = data;
-  
+
   let currentlyInfected = reportedCases * 10;
   let severeImpact = { currentlyInfected: reportedCases * 50 };
-  
+
   let powerFactor;
-  if (periodType === "days") {
+  if (periodType === 'days') {
     powerFactor = parseInt(timeToElapse / 3, 10);
-  } else if (periodType === "weeks") {
-      powerFactor = parseInt((timeToElapse * 7) / 3, 10 );
+  } else if (periodType === '"weeks"') {
+    powerFactor = parseInt((timeToElapse * 7) / 3, 10);
   } else {
-    powerFactor = parseInt((timeToElapse * 30) / 3, 10 );
+    powerFactor = parseInt((timeToElapse * 30) / 3, 10);
   }
 
-  let infectionsByRequestedTime = {
+  const infectionsByRequestedTime = {
     impact: currentlyInfected * 2 ** powerFactor,
     severeImpact: severeImpact.currentlyInfected * 2 ** powerFactor,
   };
 
-   let severeCasesByRequestedTime = parseInt((15 / 100) * infectionsByRequestedTime.severeImpact, 10);
-   let hospitalBedsByRequestedTime = 
+  const severeCasesByRequestedTime = parseInt((15 / 100) * infectionsByRequestedTime.severeImpact, 10);
+  const dedicatedBeds = parseInt((35 / 100) * totalHospitalBeds, 10);
+  const hospitalBedsByRequestedTime = dedicatedBeds - severeCasesByRequestedTime;
+
+
 };
 
 export default covid19ImpactEstimator;
