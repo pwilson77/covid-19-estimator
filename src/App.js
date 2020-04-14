f5b8ca316dbf90030da1018c190d990f52ac16c8
pwilson77 <p.o.yirenkyi@gmail.com>
2020-04-14 22:29:59 +0000

    modified App.js

--- src/App.js
+++ src/App.js
@@ -38,40 +38,35 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/v1/on-covid-19', (req, res) => {
-  const start = process.hrtime();
-  const durationInMilliseconds = getDurationInMilliseconds(start);
-
-  logger.write(`POST /api/v1/on-covid-19    ${res.statusCode}   ${durationInMilliseconds}ms \n`);
+  const start = Date.now();
   covidData = covid19ImpactEstimator(req.body);
   res.send(covidData);
+  logger.write(`${req.method} /api/v1/on-covid-19    ${res.statusCode}   ${end}ms \n`);
 });
 
 app.post('/api/v1/on-covid-19/json', (req, res) => {
-  const start = process.hrtime();
-  const durationInMilliseconds = getDurationInMilliseconds(start);
-
-  logger.write(`POST /api/v1/on-covid-19/json   ${res.statusCode}   ${durationInMilliseconds}ms \n`);
+  const start = Date.now();
   res.set('Content-Type', 'application/json');
   res.send(covidData);
+  const end = Math.floor(Date.now() - start).toString().padStart(2, 0);
+  logger.write(`${req.method} /api/v1/on-covid-19/json   ${res.statusCode}   ${end}ms \n`);
 });
 
 app.post('/api/v1/on-covid-19/xml', (req, res) => {
-  const start = process.hrtime();
-  const durationInMilliseconds = getDurationInMilliseconds(start);
-
-  logger.write(`POST /api/v1/on-covid-19/xml   ${res.statusCode}   ${durationInMilliseconds}ms \n`);
+  const start = Date.now();
   res.set('Content-Type', 'application/xml');
   res.send(toXML(covidData));
+  const end = Math.floor(Date.now() - start).toString().padStart(2, 0);
+  logger.write(`${req.method} /api/v1/on-covid-19/xml   ${res.statusCode}   ${end}ms \n`);
 });
 
 app.get('/api/v1/on-covid-19/logs', (req, res) => {
   const start = process.hrtime();
-  const durationInMilliseconds = getDurationInMilliseconds(start);
-
-  logger.write(`GET /api/v1/on-covid-19/logs   ${res.statusCode}   ${durationInMilliseconds}ms \n`);
   res.set('Content-Type', 'text/plain');
   fs.readFile('./src/log.txt', 'utf8', (err, data) => {
     if (err) throw err;
     res.send(data);
   });
+  const end = Math.floor(Date.now() - start).toString().padStart(2, 0);
+  logger.write(`${req.method} /api/v1/on-covid-19/logs   ${res.statusCode}   ${durationInMilliseconds}ms \n`);
 });
